6206f86cebf021a4a1ed8d536a47f71b61cd2ff4
Muhammad Shamim <shaminaus@gmail.com>
2022-06-08 21:24:49 -0500

    Added routes

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import config from './config/default'
 const app = express()
 const port = config.port as number
 const host = config.host as string
+const prefix = config.prefix as string
+const routes = require('./routes/index')
 
 // Define routes
-app.get('/', (req, res) => {
-    res.send('Welcome to Micro Server 2023!')
-})
+app.use(prefix, routes)
 
 // Start Server
 app.listen(port, host, () => {
-    console.log(`The application is listening at http://${host}:${port}`)
+    console.log(`The application is listening at http://${host}:${port}${prefix}`)
 })
